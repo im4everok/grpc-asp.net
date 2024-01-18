430dd7b53c654f0aff4de87bcdc84e80d821db6c
im4everok <imizyk09@gmail.com>
2024-01-18 23:50:35 +0200

    added math service, implemented Calculate()

---
+++ GrpcTests/Services/MathService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+
+namespace GrpcTests
+{
+    public class MathService : Math.MathBase
+    {
+        public MathService()
+        {
+
+        }
+
+        public override Task<MathResponse> Calculate(MathRequest request, ServerCallContext context)
+        {
+            int result = 0;
+
+            if(request.Operation == Operation.Add)
+            {
+                result = request.A + request.B;
+            }
+
+            if (request.Operation == Operation.Multiply)
+            {
+                result = request.A * request.B;
+            }
+
+            var response = new MathResponse
+            {
+                Result = result
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
