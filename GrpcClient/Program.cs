543e43a2577253952397e9dab12d58e09e8ae7e3
im4everok <imizyk09@gmail.com>
2024-01-18 23:54:36 +0200

    added proto client and sent requests to server

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+
+Console.WriteLine("Hello, World!");
+
+var client = new GrpcClient.Math.MathClient(GrpcChannel.ForAddress("https://localhost:7119"));
+
+for(int i = 1; i <= 10; i++)
+{
+    var addResult = client.Calculate(new GrpcClient.MathRequest { A = i, B = i + i, Operation = GrpcClient.Operation.Add });
+
+    var multiplyResult = client.Calculate(new GrpcClient.MathRequest { A = i, B = i + i, Operation = GrpcClient.Operation.Multiply });
+
+    Console.WriteLine($"Addition result: {addResult}. Mulptiplication: {multiplyResult}");
+}
